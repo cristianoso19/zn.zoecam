0be74c85ea56adb8226112fa6b3265f0766677d8
cristianzoe <cristianoso19@gmail.com>
2024-09-22 16:23:43 -0500

    Analitica

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 function calculoCamaras(){
     const dias = parseFloat(document.getElementById('dias').value);
     const camaras = parseFloat(document.getElementById('camaras').value);
+    const analitic = parseFloat(document.getElementById('analitica').value);
     const fulltime = 1.50
     const costoGrabacion = 0.5
+    const costoAnalitica = 1.50
     // Verificar que los valores sean números y mayores que cero
     if (isNaN(dias) || isNaN(camaras) || dias <= 0 || camaras <= 0) {
         alert('Por favor, ingrese valores válidos y mayores que cero.');
@@ -11,14 +13,35 @@ function calculoCamaras(){
         alert('Por favor, ingrese solo números enteros.');
         return;
     }
+    
+    let valor = 0;
 
-    let valor = (costoGrabacion*dias*camaras)+(fulltime*camaras)
+    if(isNaN(analitic)){
+        valor = (costoGrabacion*dias*camaras)+(fulltime*camaras)
+        console.log(`Fulltime x camaras = ${fulltime*camaras}
+Grabacion x dias x camaras = ${costoGrabacion*dias*camaras} 
+            `)
+    } else {
+        valor = (costoGrabacion*dias*camaras)+(fulltime*camaras)+(analitic*costoAnalitica)
+        console.log(`Fulltime x camaras = ${fulltime*camaras}
+Grabacion x dias x camaras = ${costoGrabacion*dias*camaras} 
+Analitica x camaras = ${analitic*costoAnalitica}
+            `)
+
+    }
+
     let total = valor + 27.50
     
      // Mostrar el resultado
     const resultado = `
-        <p class="text-xl text-slate-400 font-bold">Extra:<br/> <span class="font-bold text-xl text-slate-600">$ ${valor.toFixed(2)}</span></p>
-        <p class="text-2xl text-slate-400 font-bold">Total del Plan:<br/> <span class="font-bold text-3xl text-slate-600">$ ${total.toFixed(2)}</span></p>
+        <p class="text-xl text-slate-400 font-bold">
+            Zoecam <span>(Extra)</span>:<br/> 
+            <span class="font-bold text-xl text-slate-600">$ ${valor.toFixed(2)}</span>
+        </p>
+        <p class="text-2xl text-slate-400 font-bold">
+            Internet+Zoecam:<br/> 
+            <span class="font-bold text-3xl text-slate-600">$ ${total.toFixed(2)}</span>
+        </p>
     `;
     document.getElementById('resultado').innerHTML = resultado;
 }
@@ -28,3 +51,18 @@ function validarEntero(valor) {
     const regex = /^-?\d+$/;
     return regex.test(valor);
 }
+
+document.getElementById('analiticaCheckbox').addEventListener('change', function() {
+    const analiticaInput = document.getElementById('analitica');
+    const analiticaLabel = document.getElementById('analiticaLabel');
+
+    if (this.checked) {
+        analiticaInput.hidden = false;
+        analiticaLabel.hidden = false;
+        analiticaInput.value = NaN;
+    } else {
+        analiticaInput.hidden = true;
+        analiticaLabel.hidden = true;
+        analiticaInput.value = NaN;
+    }
+});
